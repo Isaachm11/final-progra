0606f85e65ba6c058a29d7441a7831350beb13d0
Isaac Harari <ISAAC@Isaacs-iMac.local>
2017-08-31 12:38:46 -0500

    vamos por el 100

---
+++ Numeroprimo2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Numeroprimo2
+
+{
+
+  public static void main(String[] args)
+  {
+    int a,b,acum;
+    String d;
+
+    acum=0;
+
+    Scanner np = new Scanner (System.in);
+
+    System.out.print("Dame un número mayor a 1 para ver si es primo:");
+
+    a = np.nextInt();
+
+    if (a<1) {
+        System.out.print("Verifica que el número sea mayor a 1 e intentalo de nuevo:");
+        a = np.nextInt();
+    } else {
+        for ( b=1 ; b<=a ; b++ ) {
+          if (a%b == 0) {
+            acum = acum+1;
+          }
+        }
+
+        if (acum == 2) {
+          System.out.println(a + " es número primo");
+        } else if (acum != 2) {
+          System.out.println(a + " no es un número primo");
+        }
+
+    }
+
+  }
+}
